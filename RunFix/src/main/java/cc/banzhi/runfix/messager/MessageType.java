50fd812ca6416b6096e5af07376dc91ab2870fbf
zrunker <1823132505@qq.com>
2022-03-08 15:16:29 +0800

    init Commit

--- RunFix/src/main/java/cc/banzhi/runfix/messager/MessageType.java
+++ RunFix/src/main/java/cc/banzhi/runfix/messager/MessageType.java
@@ -1,11 +1,14 @@
 package cc.banzhi.runfix.messager;
 
+import androidx.annotation.StringDef;
+
 /**
  * @program: RunFix
  * @description: 消息类型
  * @author: zoufengli01
  * @create: 2021-11-05 14:22
  **/
+@StringDef({MessageType.DOWNLOAD, MessageType.COPY_DIR, MessageType.COPY_FILE})
 public @interface MessageType {
     String DOWNLOAD = "DOWNLOAD";
     String COPY_DIR = "COPY_DIR";
